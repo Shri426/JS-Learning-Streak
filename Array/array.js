6beb617c0087b7ae463554201487ebe3c0b73a50
Shrinithi R M <144874046+Shri426@users.noreply.github.com>
2025-04-18 19:25:39 +0530

    Create array.js

---
+++ Array/array.js
@@ -0,0 +1,31 @@
+//Syntax
+let fruits = ["Apple", "Banana", "Mango"];
+
+
+//Common Methods
+push() 
+pop() 
+unshift() 
+shift() 
+length()
+includes()	
+indexOf()	
+join()	
+slice()	
+splice()	
+forEach()	
+map()	
+filter()	
+reduce()	
+reverse()	
+
+//Example Code 
+let numbers = [5, 10, 15, 20];
+// Add an item
+numbers.push(25);
+// Remove the first item
+numbers.shift();
+// Double each number
+let doubled = numbers.map(n => n * 2);
+// Filter numbers greater than 15
+let filtered = numbers.filter(n => n > 15);
