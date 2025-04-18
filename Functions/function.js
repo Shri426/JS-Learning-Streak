2ef88a4783c405c999e993e536bb424a3bc6b3f4
Shrinithi R M <144874046+Shri426@users.noreply.github.com>
2025-04-18 18:54:31 +0530

    Update function.js

--- Functions/function.js
+++ Functions/function.js
@@ -1,1 +1,26 @@
+//Function Expression
+const square = function(number){
+  return number*number;
+}
+const x=square(4);
+console.log(x);
+
+//Arrow Function
+// Syntax: let function_name=(argument 1, argument 2, ...)=>a*b
+let myFunction=(a,b)=>a*b;
+
+//Return Function
+let x = myFunction(4, 3);
+function myFunction(a, b) {
+  return a * b;
+}
+
+//Local Variables
+// code here can NOT use carName
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+}
+// code here can NOT use carName
+
 
