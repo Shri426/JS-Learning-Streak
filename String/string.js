38cc14d9add53f7b2ad0e224fa8567ed2b33ce06
Shrinithi R M <144874046+Shri426@users.noreply.github.com>
2025-04-18 19:43:00 +0530

    Create string.js

---
+++ String/string.js
@@ -0,0 +1,33 @@
+//toUpperCase() and toLowerCase()
+     let str = "hello world";
+     console.log(str.toUpperCase());  
+     console.log(str.toLowerCase());  
+
+//charAt():
+     let str = "hello";
+     console.log(str.charAt(1));  
+
+//indexOf():
+     let str = "hello";
+     console.log(str.indexOf("e"));  
+
+//slice():
+     let str = "hello";
+     console.log(str.slice(1, 4));  
+
+//replace():
+     let str = "hello";
+     console.log(str.replace("e", "a"));  
+
+//split():
+     let str = "apple,banana,orange";
+     let arr = str.split(",");
+     console.log(arr);  
+
+//trim():
+     let str = "  hello  ";
+     console.log(str.trim());  
+
+//length():
+let str = "Hello, World!";
+console.log(str.length);  
